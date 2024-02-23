1e7531c47e4b591d2855ba4f5e6cd1081f187ac3
shraddha7502 <shraddhapilodiya@gmail.com>
2024-02-23 19:00:29 +0530

    readme

---
+++ myapp/src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <main className="hero container">
+    <div className="hero-content">
+      <h2>Unleash Your Brainpower-<br/>
+       Where Brilliance Meets Badassery! 🚀</h2>
+      <h6>
+      Buckle up, legends! EnlightenHub is not your grandma's learning platform – <br/>it's a wild ride to genius town. We're not here to bore you with textbooks; <br/>we're here to ignite your mind and make learning a freakin' adventure!
+      </h6>
+
+      <div className="hero-btn">
+        <button className='btn-primary'>Get started </button>
+        <button className="secondary-btn">view courses</button>
+      </div>
+
+      <div className="shopping">
+        <p>Also Available On</p>
+
+        <div className="brand-icons">
+          <img src="/assets/girl_with_laptop.png" alt="girl" />
+          <img src="/assets/books.png" alt="books" />
+        </div>
+      </div>
+    </div>
+    <div className="hero-image">
+      <img src="assets/team.png" alt="team" />
+    </div>
+  </main>
+  )
+}
+
+export default Home
