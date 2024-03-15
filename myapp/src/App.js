a1fe3cfa3cc6a4527b2d3484eb345b83ed1f27ef
shraddha7502 <shraddhapilodiya@gmail.com>
2024-03-16 01:10:20 +0530

    read

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,12 +9,27 @@ const App = () => {
     <div>
       <Navbar/>
       <Home/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
-      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="fyuwgfwhfewhfyuewhiu"/>
+      <Card title="Python" image="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600" description="
+Elevate your programming skills with our Python course, designed to empower you with practical expertise and real-world applications"/>
+      
+      <Card title="Java developer" image="https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=600" description="Forge your path as a Java developer, crafting innovative solutions and driving technological advancements in the digital landscape"/>
+      
+      <Card title="Data analytics" image="https://images.pexels.com/photos/186464/pexels-photo-186464.jpeg?auto=compress&cs=tinysrgb&w=600" description="Unveiling actionable insights from vast datasets, driving informed decisions and strategic advancements across industries."/>
+      
+      <Card title="C++" image="https://images.pexels.com/photos/7516363/pexels-photo-7516363.jpeg?auto=compress&cs=tinysrgb&w=600" description="
+Where performance meets versatility, enabling developers to craft high-speed, scalable software solutions for diverse applications"/>
+      
+      <Card title="Master in Chatgpt" image="https://images.pexels.com/photos/16461434/pexels-photo-16461434/free-photo-of-close-up-of-a-person-holding-a-smartphone-displaying-chatgpt.jpeg?auto=compress&cs=tinysrgb&w=600" description=" Your gateway to boundless creativity, knowledge, and assistance, revolutionizing the way we interact with AI"/>
+      
+      <Card title="Computer Networking" image="https://images.pexels.com/photos/159304/network-cable-ethernet-computer-159304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" description="
+
+Fueling global connectivity, computer networks enable seamless communication and resource sharing across devices"/>
+      
+      <Card title="Artificial intelligence" image="https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=600" description="
+Harness the power of AI to amplify efficiency, drive innovation, and unlock new possibilities for businesses of all sizes"/>
+      
+      <Card title="Machine Learning" image="https://media.istockphoto.com/id/1387900612/photo/automation-data-analytic-with-robot-and-digital-visualization-for-big-data-scientist.jpg?b=1&s=612x612&w=0&k=20&c=AdtAH8A9d_UhcAJyDn6YR7XiDlf7Fz0zWRCmqQ2lSFY=" description="
+Transforming industries with data-driven insights, Machine Learning paves the way for innovation and optimization"/>
     
             
     </div>
